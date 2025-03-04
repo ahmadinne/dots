339dba2943937e7e4073dfc8625e53ec3b3534ae
veillain <fikri.mny@gmail.com>
2025-03-04 22:53:21 +0700

    Initial Commits

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -6,17 +6,27 @@ local default = { noremap = true, silent = true }
 remap("n", " ", "<nop>", { silent = true, remap = false })
 remap("n", "<bs>", "<nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
+
 -- capital U for redo
 remap("n", "U", "<c-r>")
+
 -- leader + / remove highlight
 remap("n", "<leader>/", "<cmd>noh<cr>")
 remap("n", "gh", "<cmd>noh<cr>")
+
 -- p doesn't override clipboard
 remap("x", "<leader>p", '"_dP')
 -- x doesn't override clipboard
 remap("n", "x", '"_x')
 remap("n", "X", '"_X')
+
 -- Unmap ctrl+q (visual block, same as ctrl+v)
 remap("n", "<c-q>", "", default)
 -- Escape to Esc-l
 remap("i", "<esc>", "<esc>l", default)
+
+-- Switch buffer and windows
+remap("n", "gb", "<cmd>bn<cr>")
+remap("n", "gB", "<cmd>bp<cr>")
+remap("n", "gw", "<c-w>w")
+remap("n", "gW", "<c-w>W")
