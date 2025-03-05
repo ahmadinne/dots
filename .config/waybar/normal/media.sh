33a6da43712df38d380862877ba539554f72174b
veillain <fikri.mny@gmail.com>
2025-03-06 00:56:05 +0700

    Initial Commits

--- .config/waybar/normal/media.sh
+++ .config/waybar/normal/media.sh
@@ -48,8 +48,48 @@ metadata(){
     done
 }
 
+scrolltitle(){
+    while true; do
+        status=$(playerctl status)
+        teks=$(playerctl metadata | grep title | grep -oP 'title\s+\K.*')
+        hitung=$(echo "$teks" | wc -m)
+        max_len=25
+
+        if [[ "$status" == Playing ]]; then
+            class="normal"
+            sleep 0.5
+        elif [[ "$status" == Paused ]]; then
+            class="paused"
+            sleep 0.5
+        else
+            class="normal"
+            title=$(echo $HOSTNAME)
+            artist=$(whoami)
+            sleep 5
+        fi
+
+        if [ "$hitung" -gt "$max_len" ]; then
+            teks_gabung="${teks} ${teks}"
+
+            for ((i=0; i<${#teks}; i++)); do
+                # Menampilkan potongan teks sesuai dengan panjang maksimal
+                echo -n "{\"text\": \"${teks_gabung:((i % ${#teks_gabung})):max_len}\", \"class\": \"${class}\"}" | jq --unbuffered --compact-output .
+                echo -n -e "\r"  # Menggerakkan kursor kembali ke awal baris
+                sleep 0.5  # Delay agar gerakan terlihat
+            done
+        else
+            while true; do
+                echo "{\"text\": \"$(printf "%-25s" "$teks")\", \"class\": \"${class}\"}" | jq --unbuffered --compact-output .
+                echo -n -e "\r"
+                sleep 2
+            done
+        fi
+    done
+}
+
 case $1 in
     title) metadata title ;;
     artist) metadata artist ;;
     image) image ;;
+    scrolltitle) scrolltitle ;;
 esac
