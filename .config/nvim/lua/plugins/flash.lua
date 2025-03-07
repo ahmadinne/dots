4e09ae78a732a201ef3ba9f7c23debd94e11760e
veillain <fikri.mny@gmail.com>
2025-03-07 08:14:48 +0700

    Initial Commits

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,10 +1,14 @@
 return {
-    "folke/flash.nvim",
-    config = function()
-        require("flash").setup()
-        vim.keymap.set("n", 's', function() require("flash").jump() end)
-        vim.keymap.set('n', 'S', function() require("flash").treesitter() end)
-        vim.keymap.set('n', 'f', function() require("flash").jump({ search = { forward = true, wrap = false, multi_window = false }, }) end)
-        vim.keymap.set('n', 't', function() require("flash").jump({ search = { forward = false, wrap = false, multi_window = false }, }) end)
-    end
+	"folke/flash.nvim",
+	config = function()
+		require("flash").setup()
+		-- vim.keymap.set("n", 's', function() require("flash").jump() end)
+		-- vim.keymap.set('n', 'S', function() require("flash").treesitter() end)
+		vim.keymap.set("n", "f", function()
+			require("flash").jump({ search = { forward = true, wrap = false, multi_window = false } })
+		end)
+		vim.keymap.set("n", "t", function()
+			require("flash").jump({ search = { forward = false, wrap = false, multi_window = false } })
+		end)
+	end,
 }
