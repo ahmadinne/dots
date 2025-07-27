2addec6582583ae138e88281b19a21a49afdc6c6
ahmadinne <ahmadinne@proton.me>
2025-07-27 21:59:17 +0700

    feat: update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,6 +58,7 @@ vim.pack.add({
 	{ src = "https://github.com/folke/lazydev.nvim" }, -- blink.cmp dependencies
 	{ src = "https://github.com/folke/flash.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.icons" },
 	{ src = "https://github.com/lambdalisue/vim-suda" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
@@ -66,6 +67,7 @@ vim.pack.add({
 
 -- Plugins Configurations
 vim.lsp.enable({ "lua_ls", "bashls", "ts_ls", "vls" })
+require "mini.icons".setup()
 require "mini.pick".setup()
 require "flash".setup()
 require "oil".setup({
