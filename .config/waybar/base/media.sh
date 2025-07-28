f94aa828e1c1a8fcac6cb4034a058f3b77fcc357
ahmadinne <ahmadinne@proton.me>
2025-07-28 12:32:05 +0700

    feat: update

--- .config/waybar/base/media.sh
+++ .config/waybar/base/media.sh
@@ -6,8 +6,8 @@ image(){
         cache="/tmp/mediacache"
         thumbnail="/tmp/mediathumbnail"
         fullthumb="/tmp/fullthumbnail"
-        defaultone="$HOME/.config/waybar/normal/thumbnail.png"
-        magick $artUrl -thumbnail 500x500^ -gravity center -extent 500x500 $cache
+        defaultone="$HOME/.config/waybar/base/thumbnail.png"
+        magick $artUrl -thumbnail 720x720^ -gravity center -extent 720x720 $cache
 
         if [[ "$thumbnail" != "$cache" ]]; then
             cp -rf $artUrl $fullthumb
@@ -18,36 +18,6 @@ image(){
             cp -rf $defaultone $thumbnail
         fi
         sleep 5
-    done
-}
-
-metadata(){
-    while true; do
-        status=$(playerctl status)
-        title=$(playerctl metadata | grep title | grep -oP 'title\s+\K.*')
-        artist=$(playerctl metadata | grep artist | grep -oP 'artist\s+\K.*')
-
-        if [[ "$status" == Playing ]]; then
-            class="normal"
-            sleep 0.5
-        elif [[ "$status" == Paused ]]; then
-            class="paused"
-            sleep 0.5
-        else
-            class="normal"
-            title=$(echo $HOSTNAME)
-            artist=$(whoami)
-            sleep 2
-        fi
-
-        case $1 in
-            title)
-                echo "{\"text\": \"${title}\", \"class\": \"${class}\"}" | jq --unbuffered --compact-output .
-                ;;
-            artist)
-                echo "{\"text\": \"${artist}\", \"class\": \"${class}\"}" | jq --unbuffered --compact-output .
-                ;;
-        esac
     done
 }
 
@@ -75,8 +45,6 @@ scrolltitle(){
 }
 
 case $1 in
-    title) metadata title ;;
-    artist) metadata artist ;;
     image) image ;;
     scrolltitle) scrolltitle ;;
 esac
